c8c91d4bf00f8db352f53b962e4f8e5ca5741afb
Tsila <tsilanmjr2@gmail.com>
2025-02-05 08:55:28 +0300

    feat: add default page on dashboard

---
+++ src/app/dashboard/@user/default.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DefaultUserDashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>User Dashboard</h1>
+            <p>Welcome to the default user dashboard!</p>
+        </div>
+    );
+};
+
+export default DefaultUserDashboard;
